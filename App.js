ffe5df908bf93ea0b8bb6fde360d3e4f66db447e
Jessé  Del Sole Guimarães <jessedelsoleguimaraes@MacBook-Air-de-Jesse.local>
2019-08-17 16:42:24 -0300

    First commit

---
+++ App.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    
+    <View style = {{flex:1, justifyContent:'center',alignItems:'center'}}>
+      <Text>Hello World!</Text>
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default App;
