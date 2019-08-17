36f87104279800539ca523a743f0db2ed2e02a9d
jessedelsole <42688762+jessedelsole@users.noreply.github.com>
2019-08-17 16:44:15 -0300

    Update App.js

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ const App = () => {
   return (
     
     <View style = {{flex:1, justifyContent:'center',alignItems:'center'}}>
-      <Text>Hello World!</Text>
+      <Text>Hello World!!!!</Text>
 
     </View>
   );
